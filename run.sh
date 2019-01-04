08f289b63ed147876eadd7683aa01538496c300e
youssouf1994 <youssouf1994@github.com>
2019-01-04 18:06:27 -0500

    Add latex

--- run.sh
+++ run.sh
@@ -16,6 +16,11 @@
 # Get the path of the directory containing this script and the input files
 
 dir_tests=$1
+texfile=""
+if [ ! -z "$2" ]; then
+    texfile="$2log.tex"
+fi
+
 
 echo "Ecole polytechnique"
 echo "INF421 - Design and analysis of algorithms"
@@ -35,8 +40,6 @@ underlinux=$?
 ulimit -t 30
 echo "-----"
 
-date=`date +%Y%m%d_%H%M%S`
-
 # Detecting the CPU
 if [ -e /proc/cpuinfo ] ; then
     cpuinfo=`grep "model name" /proc/cpuinfo | sort -u | cut -d ":" -f 2`
@@ -46,8 +49,7 @@ fi
 
 function Run {
   #statements
-  logfile="log-${date}.md"
-
+  logfile="log.md"
   echo -e "\nCompilation ..."
   echo -e "\nCompilation ..." >> ${logfile}
 
@@ -65,18 +67,29 @@ function Run {
   echo "Evaluation of execution time on tests."
   echo "Results can be found in $logfile."
 
-  echo "Repport : " > ${logfile}
+  echo "# Report : " > ${logfile}
 
   echo "Machine : " `hostname` "."
-  echo "### Machine : " `hostname` "."  > $logfile
+  echo "* Machine : " `hostname` "."  >> $logfile
   echo "CPU :$cpuinfo"
-  echo "### CPU :$cpuinfo"  >> $logfile
-  echo "Start date : $date."
-  echo "### Start date : $date."  >> $logfile
+  echo "* CPU :$cpuinfo"  >> $logfile
   echo "Limit of `ulimit -t` seconds per test."
-  echo "### Limit of `ulimit -t` seconds per test."  >> $logfile
+  echo "* Limit of `ulimit -t` seconds per test."  >> $logfile
   echo
   echo >> $logfile
+  if [ ! -z "$texfile" ]; then
+    echo "\\begin{itemize}" > ${texfile}
+    echo "\\item Machine : " `hostname` "."  >> ${texfile}
+    echo "\\item CPU : $cpuinfo"  >> ${texfile}
+    echo "\\item Limit of `ulimit -t` seconds per test."  >> ${texfile}
+    echo "\\end{itemize}" >> $texfile
+    echo >> $texfile
+    echo "\\begin{center}" >> $texfile
+    echo "\\begin{tabular}{|c|c|c|}">> $texfile
+    echo "\\hline" >> $texfile
+    echo "input file & weight & CPU \\\\" >> $texfile
+    echo "\\hline" >> $texfile
+  fi
 
   echo -e "| input file |weight | CPU |" >> $logfile
   echo -e "|:-----:|:-----:|:-----:|" >> $logfile
@@ -94,10 +107,17 @@ function Run {
       t=`(time -p make run < $dir_tests/$test > $dir_tests/${test%.in}.out) 2>&1 | egrep user | cut -f 2 -d " "`
     fi
     w=`cat ${dir_tests}/${test%.in}.out`
-    echo -e "${test%.in}\t${w}\t${t}"
     echo -e "| ${test%.in} | ${w} | ${t} |" >> $logfile
-
+    echo -e "${test%.in}\t${w}\t${t}"
+    if [ ! -z "$texfile" ]; then
+      echo "${test%.in} & ${w} & ${t} \\\\ " >> $texfile
+      echo "\\hline" >> $texfile
+    fi
   done
+  if [ ! -z "$texfile" ]; then
+    echo "\\end{tabular}" >> $texfile
+    echo "\\end{center}" >> $texfile
+  fi
 
 }
 
