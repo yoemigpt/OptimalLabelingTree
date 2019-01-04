0638b1b3769c59ec67e721211fa934a6f6bf475c
youssouf1994 <youssouf1994@github.com>
2019-01-03 22:15:07 -0500

    Delete OptimalTreeLabelingTest.java

--- Main.java
+++ Main.java
@@ -2,14 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-      OptimalTreeLabelingTest optimalTreeLabelingTest = new OptimalTreeLabelingTest(new OptimalTreeLabeling(), System.out);
-      for(String arg : args) {
-        try {
-          optimalTreeLabelingTest.test(arg);
-        } catch (Exception e) {
-          System.err.println("This file does not exist.");
-        }
-      }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int L = sc.nextInt();
+
+    ArrayList<LinkedList<Integer>> neighbours = new ArrayList<LinkedList<Integer>>();
+    for(int i = 0; i < N; i++) {
+        neighbours.add(new LinkedList<Integer>());
     }
+
+    for(int i = 0; i < N-1; i++) {
+        int u = sc.nextInt()-1;
+        int v = sc.nextInt()-1;
+
+        neighbours.get(u).add(v);
+        neighbours.get(v).add(u);
+    }
+
+    ArrayList<Character> labelValues = new ArrayList<Character>();
+    ArrayList<HashSet<Integer>> labels = new ArrayList<HashSet<Integer>>();
+
+    for(int i = 0; i < L; i++) {
+        int u = sc.nextInt()-1;
+        assert(neighbours.get(u).size() == 1);
+        String label = sc.next();
+        if(!label.equals("$")) {
+            for(char c : label.toCharArray()) {
+                int index = labelValues.indexOf(c);
+                if(index == -1) {
+                    HashSet<Integer> set = new HashSet<Integer>();
+                    set.add(u);
+                    labels.add(set);
+                    labelValues.add(c);
+                }
+                else {
+                    labels.get(index).add(u);
+                }
+            }
+        }
+    }
+    sc.close();
+    System.out.println((new OptimalTreeLabeling()).minimumLabelWeight(neighbours, labels));
+  }
 }
